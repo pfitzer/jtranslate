baf5c209f91e978c57a15080757cd8378ed846c3
pfitzer <michael@mp-development.de>
2018-03-04 21:23:21 +0100

    moved to argparse. only append new translations if already exist

--- README.rst
+++ README.rst
@@ -1,9 +1,10 @@
 jtranslate
 ===========
-search for JText translations in php files and generates the ini files
+search for JText translations in php files and generates the ini files. If the file exist only new translation strings will
+be appended.
 
 usage
 -----
-cli.py -s /path/to/search/directory -c [your_component]
+cli.py --source /path/to/search/directory --com [your_component]
 
 use cli.py -h to see a full list of all options
--- cli.py
+++ cli.py
@@ -1,41 +1,66 @@
-import os, re
-from optparse import OptionParser
+import argparse
+import fnmatch
+import os
+import re
 
-s = r'JText::_\((\'|"){1}(.*?)(\'|"){1}\)'
+class JTranslate(object):
 
-def read_dir(option):
-    patterns = []
-    for folder, dirs, files in os.walk(option.path, topdown=False):
-        with open(os.path.join(folder, '*.php'), 'rb') as dest:
-            for filename in files:
-                with open(os.path.join(folder, filename), 'r') as src:
-                    for l in src.readlines():
+    def __init__(self, args):
+        """
+
+        :param args:
+        """
+        self.args = args
+        self.search_pattern = r'JText::_\((\'|"){1}(.*?)(\'|"){1}\)'
+
+    def read_dir(self):
+        """
+
+        :return:
+        """
+        patterns = []
+        for folder, dirs, files in os.walk(self.args.path, topdown=False):
+            for filename in fnmatch.filter(files, '*.php'):
+                with open (os.path.join(folder, filename), 'rb') as dest:
+                    for l in dest.readlines():
                         try:
-                            pattern = re.search(s, l).group(2)
-                            if option.name.upper() in pattern:
+                            pattern = re.search(self.search_pattern, l).group(2)
+                            if self.args.com.upper() in pattern:
                                 patterns.append(pattern)
                         except:
                             continue
 
-    file_name = os.path.join(option.out, '%s.%s.ini' % (option.lang, option.name.lower()))
-    with open(file_name, 'w') as f:
-        for p in patterns:
-            f.write('%s=""\n' % p)
-    f.close()
+        self.write_file(patterns)
 
+    def write_file(self, patterns):
+        """
+
+        :param patterns:
+        :return:
+        """
+        file_name = os.path.join(self.args.out, '%s.%s.ini' % (self.args.lang, self.args.com.lower()))
+        if not os.path.isfile(file_name):
+            f = open(file_name, 'w+')
+            f.close()
+        with open(file_name, 'r+') as f:
+            for p in patterns:
+                found = any(p in line for line in f)
+                if not found:
+                    f.seek(0, os.SEEK_END)
+                    f.write('%s=""\n' % p)
+                    f.seek(0, os.SEEK_SET)
+        f.close()
 
 
 def main():
-    parser = OptionParser()
-    parser.add_option("-s", "--source", dest="path", help="directory to search in")
-    parser.add_option("-d", "--destination", dest="out", default='.', help="destination for the ini file")
-    parser.add_option("-c", "--component", dest="name", help="the name of the component")
-    parser.add_option("-l", "--language", dest="lang", default="en-GB", help="language localisation. default is en-GB")
-
-    (option, args) = parser.parse_args()
-    # if len(args) != 1:
-    #     parser.error("incorrect number of arguments. use cli.py -h for more information")
-    read_dir(option)
+    parser = argparse.ArgumentParser(description='A translation ini file generator for joomla developers')
+    parser.add_argument('--source', dest='path', help="directory to search in", required=True)
+    parser.add_argument('--com', dest='com', help="the name of the component", required=True)
+    parser.add_argument('--lang', dest='lang', default='en-GB', help="language localisation. default is en-GB")
+    parser.add_argument('--out', dest='out', default='.', help="where to save the file")
+    args = parser.parse_args()
+    jf = JTranslate(args=args)
+    jf.read_dir()
 
 
 if __name__ == "__main__":
