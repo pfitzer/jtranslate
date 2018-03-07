ffe7134b94a7095bed0b8aae12f2f9ece96a0a9e
pfitzer <michael@mp-development.de>
2018-03-07 21:34:33 +0100

    setuptools-markdown

--- setup.py
+++ setup.py
@@ -1,24 +1,18 @@
-from codecs import open
-from os.path import abspath, dirname, join
 from setuptools import setup, find_packages
 from jootranslate import __version__, __author__
 
-
-this_dir = abspath(dirname(__file__))
-with open(join(this_dir, 'README.md'), encoding='utf-8') as file:
-    long_description = file.read()
 
 setup(
     name = 'jootranslate',
     version = __version__,
     description = 'cli tool to generate translation files for joomla',
-    long_description = long_description,
+    long_description_markdown_filename='README.md',
     url = 'https://github.com/pfitzer/jtranslate.git',
     author = __author__,
     author_email = 'michael@mp-development.de',
     license = 'MIT',
     keywords = 'joomla cli translations',
-    setup_requires=['pytest-runner'],
+    setup_requires=['pytest-runner', 'setuptools-markdown'],
     tests_require=['pytest'],
     packages = find_packages(),
     entry_points = {
@@ -32,7 +26,6 @@ setup(
         'Environment :: Console',
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
