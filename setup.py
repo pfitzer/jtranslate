71b2ecec21695e3bb1818e380b857957cfa0ba42
micpfist <m.pfister@t-systems.com>
2018-03-07 14:26:57 +0100

    rst to md

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from jootranslate import __version__, __author__
 
 
 this_dir = abspath(dirname(__file__))
-with open(join(this_dir, 'README.rst'), encoding='utf-8') as file:
+with open(join(this_dir, 'README.md'), encoding='utf-8') as file:
     long_description = file.read()
 
 setup(
